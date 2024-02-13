742a5a203096249ad1926a216d7707f19f9a0be7
raoul <raoulsinde@gmail.com>
2024-02-14 00:18:08 +0100

    First commit for Black and Scholes pricing

--- black_scholes_pricing.py
+++ black_scholes_pricing.py
@@ -1,6 +1,17 @@
 from scipy.stats import norm
 from math import exp, log, sqrt
 
+"""
+This module contains the functions to calculate the Black-Scholes option pricing formula. 
+It contains the following functions:
+
+- calc_d1: calculates d1, a parameter of the equation
+- calc_d2: calculates d2, a parameter of the equation
+- call_price: calculates the price of a call option
+- put_price: calculates the price of a put option
+
+"""
+
 
 def calc_d1(s0, k, r, t, sigma):
     return (1 / (sigma * sqrt(t))) * (log(s0 / k) + (r + (sigma ** 2) / 2) * t)
--- options_pricing.py
+++ options_pricing.py
@@ -2,13 +2,30 @@ import black_scholes_pricing as bsp
 
 
 class Option:
+    """
+    This class defines an Option that has different characteristics:
+        s0 is the price of the underlying asset
+        k is the strike price
+        r is the risk-free interest rate
+        t is the time to maturity in years
+        sigma is the volatility of the underlying asset
+        option_type is the type of the option (call or put)
+
+    The class has also a method called price that returns the price of the option:
+        to do so a method calculates d1 and d2 that are used in the Black-Scholes formula
+
+    The formulas are implemented in the black_scholes_pricing.py file that is called in the method price
+    and calc_d1_d2 of this class
+
+    """
     def __init__(self, s0, k, r, t, sigma, option_type):
         self.s0 = s0
         self.k = k
         self.r = r
         self.t = t
-        self.sigma = sigma
         self.option_type = option_type
+        self.sigma = sigma
+
         self.d1, self.d2 = self.calc_d1_d2()
 
     def calc_d1_d2(self):
@@ -17,10 +34,10 @@ class Option:
 
         return d1, d2
 
-    def call_price(self):
+    def price(self):
 
-        return bsp.call_price(self.s0, self.k, self.r, self.t, self.d1, self.d2)
+        if self.option_type == 'call':
+            return bsp.call_price(self.s0, self.k, self.r, self.t, self.d1, self.d2)
 
-    def put_price(self):
-
-        return bsp.put_price(self.s0, self.k, self.r, self.t, self.d1, self.d2)
+        elif self.option_type == 'put':
+            return bsp.put_price(self.s0, self.k, self.r, self.t, self.d1, self.d2)
