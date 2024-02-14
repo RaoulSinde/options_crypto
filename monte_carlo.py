f61ff3e7ac604cc0dbfcc323ffdbba764d781d26
raoul <raoulsinde@gmail.com>
2024-02-14 13:09:04 +0100

    Simplification of pricing method

--- monte_carlo.py
+++ monte_carlo.py
@@ -3,31 +3,24 @@ import random
 import matplotlib.pyplot as plt
 from math import sqrt
 
-mc_sims = 200
-T = 180
-
-c0 = 100
-er = 0.1 / 365
-vol = 0.15 / sqrt(365)
-k = 110
-r = 0.02
-t = 1 / 2
-sigma = 0.15
-
 colors = ['r', 'g', 'b', 'y']
 
 
-def monte_carlo_bs():
+def monte_carlo_bs(s0, er, vol, sims=200, horizon=180):
+
+    er = er / 365
+    vol = vol / sqrt(365)
+
     plt.ion()
     fig, ax = plt.subplots()
     ax.set_xlabel("Time")
     ax.set_ylabel("Asset price")
-    ax.set_title("Monte Carlo simulation")
+    ax.set_title("Monte Carlo simulations")
 
-    for sim in range(mc_sims):
-        asset_price = [c0]
+    for sim in range(sims):
+        asset_price = [s0]
 
-        for day in range(1, T):
+        for day in range(1, horizon):
             # call_price = bs.price(asset_price[-1], k, r, t-day/365, sigma, 'call')
 
             asset_price.append(asset_price[-1] * (1 + random.gauss(er, vol)))
@@ -38,6 +31,3 @@ def monte_carlo_bs():
     plt.ioff()
     plt.legend()
     plt.show()
-
-
-monte_carlo_bs()
