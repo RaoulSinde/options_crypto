145c3a27d65617956d20098dc805e00444fcd996
raoul <raoulsinde@gmail.com>
2024-02-14 09:46:42 +0100

    Black and Scholes pricing

--- black_scholes.py
+++ black_scholes.py
@@ -13,19 +13,19 @@ It contains the following functions:
 """
 
 
-def calc_d1(s0, k, r, t, sigma):
-    return (1 / (sigma * sqrt(t))) * (log(s0 / k) + (r + (sigma ** 2) / 2) * t)
+def calc_d1(s, k, r, t, sigma):
+    return (1 / (sigma * sqrt(t))) * (log(s / k) + (r + (sigma ** 2) / 2) * t)
 
 
 def calc_d2(d1, t, sigma):
     return d1 - sigma * sqrt(t)
 
 
-def call_price(s0, k, r, t, d1, d2):
+def call_price(s, k, r, t, d1, d2):
     norm_dist = norm(0, 1)
-    return s0 * norm_dist.cdf(d1) - k * exp(- r * t) * norm_dist.cdf(d2)
+    return s * norm_dist.cdf(d1) - k * exp(- r * t) * norm_dist.cdf(d2)
 
 
-def put_price(s0, k, r, t, d1, d2):
+def put_price(s, k, r, t, d1, d2):
     norm_dist = norm(0, 1)
-    return - s0 * norm_dist.cdf(- d1) + k * exp(- r * t) * norm_dist.cdf(- d2)
+    return - s * norm_dist.cdf(- d1) + k * exp(- r * t) * norm_dist.cdf(- d2)
--- options_pricing.py
+++ options_pricing.py
@@ -1,10 +1,10 @@
-import black_scholes_pricing as bsp
+import black_scholes as bs
 
 
 class Option:
     """
     This class defines an Option that has different characteristics:
-        s0 is the price of the underlying asset
+        s is the price of the underlying asset
         k is the strike price
         r is the risk-free interest rate
         t is the time to maturity in years
@@ -18,8 +18,8 @@ class Option:
     and calc_d1_d2 of this class
 
     """
-    def __init__(self, s0, k, r, t, sigma, option_type):
-        self.s0 = s0
+    def __init__(self, s, k, r, t, sigma, option_type):
+        self.s = s
         self.k = k
         self.r = r
         self.t = t
@@ -29,15 +29,15 @@ class Option:
         self.d1, self.d2 = self.calc_d1_d2()
 
     def calc_d1_d2(self):
-        d1 = bsp.calc_d1(self.s0, self.k, self.r, self.t, self.sigma)
-        d2 = bsp.calc_d2(d1, self.t, self.sigma)
+        d1 = bs.calc_d1(self.s, self.k, self.r, self.t, self.sigma)
+        d2 = bs.calc_d2(d1, self.t, self.sigma)
 
         return d1, d2
 
     def price(self):
 
         if self.option_type == 'call':
-            return bsp.call_price(self.s0, self.k, self.r, self.t, self.d1, self.d2)
+            return bs.call_price(self.s, self.k, self.r, self.t, self.d1, self.d2)
 
         elif self.option_type == 'put':
-            return bsp.put_price(self.s0, self.k, self.r, self.t, self.d1, self.d2)
+            return bs.put_price(self.s, self.k, self.r, self.t, self.d1, self.d2)
