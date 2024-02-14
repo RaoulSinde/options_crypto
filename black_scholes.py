f635e00a97c3717ea2809f31dfaead02f488eedd
raoul <raoulsinde@gmail.com>
2024-02-15 00:32:23 +0100

    Change monte carlo

--- black_scholes.py
+++ black_scholes.py
@@ -6,7 +6,15 @@ from math import exp, log, sqrt
 
 
 def price(st, k, r, t, sigma, option_type):
-
+    """ This function calculates the price of an option using the Black-Scholes formula
+    :param st: price of the underlying asset
+    :param k: strike price
+    :param r: risk-free interest rate
+    :param t: time to maturity in years
+    :param sigma: volatility of the underlying asset
+    :param option_type: type of the option (call or put)
+    :return: price of the option
+    """
     norm_dist = norm(0, 1)
     d1 = (1 / (sigma * sqrt(t))) * (log(st / k) + (r + (sigma ** 2) / 2) * t)
     d2 = d1 - sigma * sqrt(t)
--- monte_carlo.py
+++ monte_carlo.py
@@ -1,12 +1,12 @@
 import random
+import numpy as np
 import black_scholes as bs
-import matplotlib.pyplot as plt
 from math import sqrt
 
 colors = ['r', 'g', 'b', 'y']
 
 
-def monte_carlo_bs(s0, k, r, t, sigma, option_type, er, sims=200, horizon=180):
+def monte_carlo_bs(s0, k, r, t, sigma, option_type, er, sims=100, horizon=100):
     """ This function calculates Monte Carlo simulations for the price of options
     :param s0: price of the underlying asset
     :param k: strike price
@@ -17,28 +17,20 @@ def monte_carlo_bs(s0, k, r, t, sigma, option_type, er, sims=200, horizon=180):
     :param er: yearly expected return for the underlying asset
     :param sims: number of simulations
     :param horizon: number of days of simulations (number of simulated prices)
+    :return: array of option prices for each simulation
     """
     er = er / 365
     vol = sigma / sqrt(365)
 
-    plt.ion()
-    fig, ax = plt.subplots()
-    ax.set_xlabel("Time")
-    ax.set_ylabel("Call price")
-    ax.set_title("Monte Carlo simulations")
+    asset_prices = np.zeros((sims, horizon))
+    option_prices = np.zeros((sims, horizon))
 
     for sim in range(sims):
-        asset_price = [s0]
-        option_price = [bs.price(s0, k, r, t, sigma, option_type)]
+        asset_prices[sim, 0] = s0
+        option_prices[sim, 0] = bs.price(s0, k, r, t, sigma, option_type)
 
         for day in range(1, horizon):
+            asset_prices[sim, day] = asset_prices[sim, day - 1] * (1 + random.gauss(er, vol))
+            option_prices[sim, day] = bs.price(asset_prices[sim, day], k, r, t - day / 365, sigma, option_type)
 
-            asset_price.append(asset_price[-1] * (1 + random.gauss(er, vol)))
-            option_price.append(bs.price(asset_price[-1], k, r, t-day/365, sigma, option_type))
-
-        random_index = random.randint(0, len(colors) - 1)
-        ax.plot(option_price, color=colors[random_index], linewidth=0.5)
-
-    plt.ioff()
-    plt.legend()
-    plt.show()
+    return option_prices
